e5d404ca19847ea0aa83e1de66c9c7fef6b48863
todhm <todhm@nate.com>
2024-02-12 12:45:50 +0900

    fix real estate data

--- datatypes/openapi.py
+++ datatypes/openapi.py
@@ -1,4 +1,6 @@
-from typing import TypedDict, Optional, Literal
+from typing import Optional, Literal
+from typing_extensions import TypedDict
+from datetime import datetime as dt
 
 
 class RealEstate(TypedDict):
@@ -8,19 +10,20 @@ class RealEstate(TypedDict):
     SGG_NM: str
     BJDONG_CD: str
     BJDONG_NM: str
-    LAND_GBN: str
-    LAND_GBN_NM: str
-    BONBEON: str
-    BUBEON: str
-    BLDG_NM: str
+    LAND_GBN: Literal["1", "2", "3", None]
+    LAND_GBN_NM: Literal["대지", "산", "블럭", None]
+    BONBEON: Optional[str]
+    BUBEON: Optional[str]
+    BLDG_NM: Optional[str]
     DEAL_YMD: str
+    DEAL_YMD_DT: dt
     OBJ_AMT: float
     BLDG_AREA: float
     TOT_AREA: float
-    FLOOR: int
-    RIGHT_GBN: str
+    FLOOR: Optional[int]
+    RIGHT_GBN: Optional[str]
     CNTL_YMD: Optional[str]
-    BUILD_YEAR: int
-    HOUSE_TYPE: Literal['아파트']
-    REQ_GBN: Literal['중개거래']
-    RDEALER_LAWDNM: str
+    BUILD_YEAR: Optional[int]
+    HOUSE_TYPE: Literal['아파트', '연립다세대', '오피스텔', '단독다가구']
+    REQ_GBN: Literal['중개거래', '직거래']
+    RDEALER_LAWDNM: Optional[str]
--- openapi/openapi.py
+++ openapi/openapi.py
@@ -1,32 +1,41 @@
 import requests
-import xmltodict 
+from datetime import datetime as dt
+import xmltodict
 from typing import List
 from fp_common.datatypes.openapi import RealEstate
 from copy import deepcopy
 
 
 def fetch_realestate_total_counts() -> int:
-    url = 'http://openapi.seoul.go.kr:8088/sample/xml/tbLnOpendataRtmsV/0/5/'
+    url = "http://openapi.seoul.go.kr:8088/sample/xml/tbLnOpendataRtmsV/0/5/"
     results = xmltodict.parse(requests.get(url).text)
-    counts = int(results['tbLnOpendataRtmsV']['list_total_count'])
+    counts = int(results["tbLnOpendataRtmsV"]["list_total_count"])
     return counts
-    
 
-def fetch_realestate_data(api_key: str, start_number: int, end_number: int) -> List[RealEstate]:
-    url = f'http://openapi.seoul.go.kr:8088/{api_key}/xml/tbLnOpendataRtmsV/{start_number}/{end_number}/'
+
+def fetch_realestate_data(
+    api_key: str, start_number: int, end_number: int
+) -> List[RealEstate]:
+    url = f"http://openapi.seoul.go.kr:8088/{api_key}/xml/tbLnOpendataRtmsV/{start_number}/{end_number}/"
     results = xmltodict.parse(requests.get(url).text)
-    data_list = results['tbLnOpendataRtmsV']['row']
+    data_list = results["tbLnOpendataRtmsV"]["row"]
     row_list = []
     for row in data_list:
         new_row = deepcopy(row)
-        new_row['id'] = (
+        new_row["id"] = (
             f"{new_row['ACC_YEAR']}-{new_row['SGG_CD']}-{new_row['BJDONG_CD']}-{new_row['LAND_GBN']}-"
             f"{new_row['BONBEON']}-{new_row['FLOOR']}-{new_row['DEAL_YMD']}"
         )
-        new_row['ACC_YEAR'] = int(new_row['ACC_YEAR'])
-        new_row['OBJ_AMT'] = float(new_row['OBJ_AMT'])
-        new_row['BLDG_AREA'] = float(new_row['BLDG_AREA'])
-        new_row['TOT_AREA'] = float(new_row['TOT_AREA'])
-        new_row['FLOOR'] = int(new_row['FLOOR']) if new_row.get("FLOOR") else None
+        new_row["ACC_YEAR"] = int(new_row["ACC_YEAR"])
+        new_row["OBJ_AMT"] = float(new_row["OBJ_AMT"]) * 10000
+        new_row["BLDG_AREA"] = float(new_row["BLDG_AREA"])
+        new_row["TOT_AREA"] = float(new_row["TOT_AREA"])
+        new_row["DEAL_YMD_DT"] = dt.strptime(new_row["DEAL_YMD"], "%Y%m%d")
+        new_row["FLOOR"] = int(new_row["FLOOR"]) if new_row.get("FLOOR") else None
+        new_row["BUILD_YEAR"] = (
+            int(new_row["BUILD_YEAR"])
+            if new_row.get("BUILD_YEAR") and new_row.get("BUILD_YEAR") != "null"
+            else None
+        )
         row_list.append(new_row)
     return row_list
