aff8f5dec5fb6617ad4c7f0701aebb9b4c02b4f3
heemyung <heemyung@heemyung.local>
2024-10-20 15:36:28 +0900

    feat: 저작권관련 담보 추가

---
+++ fp_types/enums/p2p/company.py
@@ -0,0 +1,10 @@
+from fp_common.fp_types.enums.base import BaseEnum
+
+
+class CompanyName(BaseEnum):
+    CREDIT_PLANET = "CREDIT_PLANET"
+    EIGHT_PERCENT = '8 percent'
+    HI_FUNDING = 'Hi Funding'
+    COCKTAIL_FUNDING = 'Coctail Funding'
+    WE_FUNDING = 'We Fund'
+    FUNDING_119 = 'Funding 119'
---
+++ fp_types/enums/p2p/product_type.py
@@ -0,0 +1,11 @@
+from fp_common.fp_types.enums.base import BaseEnum
+
+
+class ProductType(BaseEnum):
+    STOCK = "STOCK"
+    MORTGAGE = 'MORTGAGE'
+    COMPLEX_MORTGAGE = 'COMPLEX_MORTGAGE'
+    PF = "PF"
+    PERSONAL = "PERSONAL"
+    COPYRIGHT = "COPYRIGHT"
+    SALES = "SALES"
