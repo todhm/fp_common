c2ab96c5d85a902faaab9e655e0a6fd720dca2e7
heemyung <heemyung@heemyung.local>
2024-11-02 22:50:06 +0900

    feat: 크플용상품추가

---
+++ fp_types/p2pproducts/credit_planet.py
@@ -0,0 +1,31 @@
+from typing import TypedDict, Literal, Optional, Dict, List
+from fp_common.fp_types.enums.p2p.product_type import ProductType
+
+
+class CreditPlanetLinkDict(TypedDict):
+    type: Literal["mortgage", "stock"]
+    funding_amount: int
+    funding_end_datetime: Optional[str]
+    funding_start_datetime: str
+    icon_url: str
+    id: int
+    is_closed: bool
+    is_investable: bool
+    is_notified_open_product: bool
+    name: str
+    product_type: str
+    properties: List[Dict]
+    raised_amount: int
+    raised_rate: float
+    repayment_type: str
+    status: str
+
+
+class PagePropDict(TypedDict):
+    pageProps: Dict
+
+
+class CreditPlanetProductDict(TypedDict):
+    props: PagePropDict
+    page: str
+    query: Dict
